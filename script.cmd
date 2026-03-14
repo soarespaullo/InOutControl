4714f42cf6c1501a94b20dc3fb8809a36cfb3477
Paulo Soares <soarespaullo@riseup.net>
2026-03-14 12:26:52 -0300

    Atualizar o script.cmd

--- script.cmd
+++ script.cmd
@@ -1,6 +1,12 @@
 @echo off
 cd /d C:\InOutControl
-call venv\Scripts\activate 
+call venv\Scripts\activate
+
+:: Inicia o python em uma nova janela.
+start /b python app.py
+
+:: Espera 3 segundos (ajuste conforme sua necessidade)
+timeout /t 3 /nobreak
+
+:: Abre o navegador
 start http://127.0.0.1:5000
-python app.py
-pause
