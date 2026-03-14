ebd8e22f89975d9b227c7f4f3b1aaa86a27e0880
Paulo Soares <soarespaullo@riseup.net>
2026-03-14 12:04:15 -0300

    Atualizar o script.cmd

--- script.cmd
+++ script.cmd
@@ -1,4 +1,6 @@
 @echo off
-Ou cd /d C: \InOutControl call venv\Scripts\activate start http://127.0.0.1:5000
+Ou cd /d C: \InOutControl
+call venv\Scripts\activate 
+start http://127.0.0.1:5000
 python app.py
 pause
